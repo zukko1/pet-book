f5ef66450c45fef722d358ca9320ac92e5cb6aec
Carlos Acevedo <cacevedod@devco.com.co>
2020-03-22 14:56:25 -0500

    scripts install sonarqube

---
+++ sonarqube/init.sh
@@ -0,0 +1,11 @@
+# Install docker
+sudo sh get-docker.sh
+
+# Install dockercompose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+
+#Install sonarqube
+docker compose up
