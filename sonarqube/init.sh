deced48e75955ebd6aeb3b46e80abf5ff285f6db
Carlos Acevedo <cacevedod@devco.com.co>
2020-03-22 15:25:45 -0500

    ajustes al script

--- sonarqube/init.sh
+++ sonarqube/init.sh
@@ -1,11 +1,13 @@
 # Install docker
-sudo sh get-docker.sh
+sudo yum install -y docker
+sudo systemctl start docker
+sudo systemctl enable docker
 
 # Install dockercompose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-docker-compose --version
+sudo docker-compose --version
 
 #Install sonarqube
-docker compose up
+sudo docker-compose up -d
