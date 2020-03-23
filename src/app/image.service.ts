2399156e124c5a5f56ef8ab92862abb5c706fcef
Carlos Acevedo <cacevedod@devco.com.co>
2020-03-23 13:10:36 -0500

    ajuste del perro, estaba mal creado

--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -16,5 +16,5 @@ const Imagesdelatils = [
     { "id": 2, "brand": "perro", "url": "assets/images/perro2.jpg" },
     { "id": 3, "brand": "gato", "url": "assets/images/gato1.jpg" },
     { "id": 4, "brand": "gato", "url": "assets/images/gato2.jpeg" },
-    { "id": 5, "brand": "gato", "url": "assets/images/perro3.jpeg" },
+    { "id": 5, "brand": "perro", "url": "assets/images/perro3.jpg" },
 ]  
