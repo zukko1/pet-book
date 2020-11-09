b44b0ab1c9a1dd549c68028447864fffb3b810ee
Carlos Acevedo <cacevedod@devco.com.co>
2020-11-09 14:27:11 -0500

    ajuste de chrome para que funcione OK en el contenedor de jenkins

--- karma.conf.js
+++ karma.conf.js
@@ -6,7 +6,13 @@ process.env.CHROME_BIN = require('puppeteer').executablePath()
 module.exports = function (config) {
   config.set({
     basePath: '',
-    browsers: ['ChromeHeadless'],
+    browsers: ['HeadlessChrome'],
+    customLaunchers:{
+      HeadlessChrome:{
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
